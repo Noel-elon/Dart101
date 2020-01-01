54c91e9becc570795c81b05b5af34650c1759392
Noel-elon <noelnwaelugo@gmail.com>
2020-01-01 12:27:09 +0100

    Commit

--- Practice/FirstDart.dart
+++ Practice/FirstDart.dart
@@ -1,14 +1,24 @@
 import 'dart:core';
 import 'dart:io';
 
+///@author: Nwaelugo Noel
+///noelnwaelugo@gmail.com
+main() {
+  //just like scanners in java
+  //stdout.writeln('what is your name?');
+  // var name = stdin.readLineSync();
+  //print("my name is $name");
 
+  //you can use var for any data type
 
+  // conversion between data types
+  var one = int.parse('13');
+  if (one == 23)
+    print('true');
+  else if(one == 13)
+    print('false');
+  else
+    print('Noel');
 
-main (){
-  stdout.writeln('what is your name?');
-  var name = stdin.readLineSync();
-  print("my name is $name");
-
-  var firstname = "Niel";
 
 }
