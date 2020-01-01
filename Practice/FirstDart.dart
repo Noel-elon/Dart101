21cf548b1151400ab91b3fe74e3b8bc4365ecc92
Noel-elon <noelnwaelugo@gmail.com>
2020-01-01 09:43:19 +0100

    Initial commit

---
+++ Practice/FirstDart.dart
@@ -0,0 +1,14 @@
+import 'dart:core';
+import 'dart:io';
+
+
+
+
+main (){
+  stdout.writeln('what is your name?');
+  var name = stdin.readLineSync();
+  print("my name is $name");
+
+  var firstname = "Niel";
+
+}
