603384efd878f9649183dabfb1979672531aaf8a
Noel-elon <noelnwaelugo@gmail.com>
2020-01-02 09:00:45 +0100

    continued

--- Practice/FirstDart.dart
+++ Practice/FirstDart.dart
@@ -15,10 +15,59 @@ main() {
   var one = int.parse('13');
   if (one == 23)
     print('true');
-  else if(one == 13)
+  else if (one == 13)
     print('false');
   else
     print('Noel');
 
+  int x = 100;
+  //Ternary operator... kinda like a conditional..
+  var result = x % 2 == 0 ? 'Even' : 'Odd';
+  print(result);
 
+//type test
+  var y = 40;
+  if (y is int) print('true');
+
+  //Conditional statements
+  //if else if, else, switch
+
+  //Loops
+  for (int i = 0; i <= 10; i++) print(i);
+  //for in loop
+  var numbers = [1, 2, 3, 4];
+  for (var n in numbers) {
+    print(n);
+  }
+  //for each loop
+  numbers.forEach((n) => print(n));
+
+  //while loop
+  int g = 5;
+  while (g < 20) {
+    print(g);
+    g++;
+  }
+  //do while
+
+  //break and continue
+  for (var i = 0; i < 10; i++) {
+    if (i > 5) break;
+    print(i);
+  }
+  //converting to string
+  var num = 23.toString();
+  const num2 = 45;
+
+  var n = Num();
+  int nmber;
+  //question mark after the variable name is used to check for nulPointer Exceptions
+  //the double queston mark is used to assign a default value
+  nmber = n?.num ?? 0;
+//the ??= operator is used to assign a value to a variable if it is null
+  nmber ??= 100;
+}
+
+class Num {
+  int num = 10;
 }
